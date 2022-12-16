0bc93bf1687c8fc72a04eb1aa97b27ddb8c13858
BenLin77 <goldking521@gmail.com>
2022-12-16 11:06:26 +0800

    adding time.

--- api/index.py
+++ api/index.py
@@ -50,7 +50,7 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="我可以說話囉，歡迎來跟我互動 ^_^ "))
-            print(now.hours)
+        print(now.hours)
         return
 
     if event.message.text == "柴柴閉嘴":
