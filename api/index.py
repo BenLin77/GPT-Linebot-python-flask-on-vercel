736919acf3aef28dbf23d09e731237380c5592ba
BenLin77 <goldking521@gmail.com>
2022-12-17 09:46:08 +0800

    adding time.

--- api/index.py
+++ api/index.py
@@ -70,7 +70,7 @@ def handle_message(event):
 
     if working_status and event.message.text.startswith('柴柴',0, 4):
         print(now_hour+'===')
-        if str(now_hour) == 1 or (now_hour) == 18:
+        if now_hour == 1 or now_hour == 18:
             chatgpt.add_msg(f"HUMAN:{event.message.text}?\n")
             reply_msg = chatgpt.get_response().replace("AI:", "", 1)
             chatgpt.add_msg(f"AI:{reply_msg}\n")
