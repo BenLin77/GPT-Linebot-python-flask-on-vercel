c19923e76ac7f93e086224f16e10ff3b056ff945
BenLin77 <goldking521@gmail.com>
2023-01-16 23:26:50 +0800

    Fix the YT bug.

--- api/index.py
+++ api/index.py
@@ -78,6 +78,7 @@ def handle_message(event):
 
     if event.message.text.upper().startswith('CALL', 0, 4):
         result = findYT(' ',join(event.message.text.split()[1:])
+        print(result)
         YT_link = 'https://www.youtube.com/watch?v=' + result
         line_bot_api.reply_message(
             event.reply_token,
